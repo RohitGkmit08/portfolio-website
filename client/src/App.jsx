4b5d77a933d9ea17c79e9b36742388da1ed5ebda
Rohit Sinha <rohitsinha0812@gmail.com>
2026-07-07 13:18:45 +0530

    feat: initialize client boilerplate and styling structure

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <section className="light">
+        <div className="container">
+          <h1>Portfolio</h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
